64dca9329bb0943b76b7309c67b288ed92356bcb
hiroto-hash <hiroto5118@icloud.com>
2020-12-04 15:23:10 +0900

    index

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux'
-import { Provider } from 'react-redux' 
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 import './index.css';
-import App from './components/App';
+import EventIndex from './components/event_index';
 import reducer from './reducer'
 import reportWebVitals from './reportWebVitals';
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunk))
 
-ReactDOM.render( 
+ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <EventIndex />
   </Provider>,
   document.getElementById('root')
 );
