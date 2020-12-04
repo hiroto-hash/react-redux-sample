f6ac739065b64436610984c00303f2148a67a991
hiroto-hash <hiroto5118@icloud.com>
2020-12-04 12:00:24 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 
-class App extends Component {
+const App = () => (<Continer></Continer>)
+class Continer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { count:0 }
+    console.log(props)
+  }
+  handlePlus = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
+  handleMinus = () => {
+    this.setState({ count: this.state.count - 1})
+  }
   render() {
+    console.log(this.state)
     return (
       <div>
-        <h1>Hello</h1>
+        <div>count : { this.state.count }</div>
+        <button onClick={this.handlePlus}>+1</button>
+        <button onClick={this.handleMinus}>-1</button>
       </div>
     )
   }
